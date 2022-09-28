7f9842f46e8450f0cffac46f3d71c12cf8c1c679
michaelroesinger <mroesinger@gmail.com>
2022-09-27 20:17:20 -0700

    commit tree 8d40dd8b3451369e84abe6aa7adadd1e8d789d89

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,11 @@
+class CampersController < ApplicationController
+
+    # def index
+    #     index_camper = Camper.find_by(id: params[:id])
+    #     if index_camper
+    #         render json: index_camper, status: :ok
+    #     else
+    #         render json: {error: "apartment not found"}, status: :not_found
+    #     end
+    # end
+end
---
+++ app/models/signup.rb
@@ -0,0 +1,5 @@
+class Signup < ApplicationRecord
+    belongs_to :camper
+    belongs_to :activity
+    validates :time, numericality: {in: 0...23}
+end
